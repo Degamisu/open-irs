48dce7657280765b45516c359d88e8d6bf5adc80
Degamisu <degamisu@gmail.com>
2024-01-29 17:54:25 -0500

    fix

--- index.js
+++ index.js
@@ -7,34 +7,29 @@ module.exports = (app) => {
   app.log.info("Yay, the app was loaded!");
 
   app.on("issues.opened", async (context) => {
-    const { username, repository } = context.payload.sender.login;
-  
+    const { login: username } = context.payload.sender;
+    const { full_name: repository } = context.payload.repository;
+
     const issueComment = context.issue({
       body: `
-  OpenIRS bot by \`Emi Yamashita\` at \`Degamisu\`.
-  
-  Hello, ${username}. Thank you for opening an issue at ${repository}!
-  
-  Some info for the developer:
-  
-  \`\`\`plaintext
-  Name: ${username}
-  Repository: ${repository}
-  \`\`\
 
-  ---
+OpenIRS bot by \`Emi Yamashita\` at \`Degamisu\`.
 
-  _This is an automated message from [open-irs](https://github.com/Degamisu/open-irs). Some data may be inacurate._
+Hello, ${username}. Thank you for opening an issue at ${repository}!
 
-  `
+Some info for the developer:
+
+\`\`\`plaintext
+Name: ${username}
+Repository: ${repository}
+\`\`\`
+
+---
+
+_This is an automated message from [open-irs](https://github.com/Degamisu/open-irs). Some data may be inaccurate._
+`
     });
-  
+
     return context.octokit.issues.createComment(issueComment);
-  });  
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  });
 };
